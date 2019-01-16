594a026e1ed2944f7f27044b4e349a462da4efb4
Christopher Davis <brainblasted@disroot.org>
2019-01-16 18:52:30 -0500

    mainWindow: set a default title for zero sounds recorded
    Previously we set the window's title to "" when there were no
    sounds recorded. On some desktops, this caused an issue where
    sound recorder was labeled "Untitled Window".
    
    Now Sound Recorder sets the title to "No Recorded Sounds".
    
    Fixes https://gitlab.gnome.org/GNOME/gnome-sound-recorder/issues/34

--- src/mainWindow.js
+++ src/mainWindow.js
@@ -368,7 +368,7 @@ var MainView = new Lang.Class({
                                      "%d Recorded Sounds",
                                       sounds).format(sounds);
         } else {
-            title = "";
+            title = _("No Recorded Sounds");
         }
         header.set_title(title);
 
